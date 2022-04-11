4ced397024da9eadbac180399e049ef4ac0399b4
Abdulmajeed98 <abdulmajeedjaafer18@gmail.com>
2022-04-11 15:18:20 +0300

    adjust height and width of loading indicator

--- src/components/global/LoadingIndicator.jsx
+++ src/components/global/LoadingIndicator.jsx
@@ -3,7 +3,7 @@ import ReactLoading from "react-loading";
 const LoadingIndicator = () => {
   return (
     <div className="flex h-full w-full items-center justify-center gap-4 text-3xl">
-      <ReactLoading type="spinningBubbles" className="h-3 w-3" />
+      <ReactLoading type="spinningBubbles" height={"50px"} width={"50px"} />
       Loading...
     </div>
   );
